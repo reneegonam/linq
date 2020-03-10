35141ccf34aec802c23ac683ba8577dcb081256d
Renee Alexander <reneegonzalez2008@gmail.com>
2020-03-10 14:54:54 -0400

    Agregar archivos de proyecto.

---
+++ WinFormsLinq/Conectar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsLinq
+{
+    class Conectar
+    {
+        LinqEntities BD = new LinqEntities();
+        
+
+        //Realizar una consulta simple
+        public personales  ConsultaSimple(int id)
+        {
+            var resultado = (from x in BD.personales
+          
+                            select x).FirstOrDefault ();
+
+            return resultado;
+           
+        }
+    }
+}
